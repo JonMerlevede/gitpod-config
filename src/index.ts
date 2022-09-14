5460f936ad46d489207c9e7a637075710ef5af2c
Jonathan Merlevede <jonathan@merleve.de>
2022-09-14 17:47:01 +0200

    Add YAML endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import allTasks from "./tasks.json"
+import taskRepo from "./tasks.json"
 
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
@@ -21,7 +21,32 @@ export interface Env {
 	// MY_BUCKET: R2Bucket;
 }
 
-function buildScript(url: string, actions: Array<[string, string]>): string {
+function generateTaskElementYaml(name: string, tasks: Array<[string, string]>): string[] {
+	if (tasks.length < 1) {
+		return []
+	}
+	let result = [`${name}: |`]
+	tasks.forEach(([task, taskContent]) => {
+		result.push(`  # executing ${task}`)
+		result.push(...taskContent.split("\n").map(line => "  " + line))
+	})
+	return result
+}
+function generateYaml(
+	url: string,
+	beforeCommands: Array<[string, string]>,
+	initCommands: Array<[string, string]>,
+	commandCommands: Array<[string, string]>): string {
+	let result: Array<string> = []
+	result.push("tasks:")
+	result.push(`  - name: Gitpod config (generated at ${url})`)
+	result.push(...generateTaskElementYaml("before", beforeCommands).map(line => "    " + line))
+	result.push(...generateTaskElementYaml("init", initCommands).map(line => "    " + line))
+	result.push(...generateTaskElementYaml("command", commandCommands).map(line => "    " + line))
+	return result.join("\n")
+}
+
+function generateTaskElementBash(url: string, commands: Array<[string, string]>): string {
 	const { protocol, host, searchParams } = new URL(url)
 	const base = `${protocol}//${host}`
 	const tasks = searchParams.get("tasks")
@@ -39,11 +64,39 @@ tasks:
 
 END
 
-${actions.map(([task, taskContent]) => `# executing ${task}
+${commands.map(([task, taskContent]) => `# executing ${task}
 ${taskContent}
 `
 	).join("")}
 	`
+}
+
+function getCommandsFromRepo(taskType: string, taskNames: string[]): Array<[string, string]> {
+	const tasks: [string, string][] = []
+	taskNames.forEach((taskName) => {
+		if ((taskName in taskRepo) && (taskType in (<any>taskRepo)[taskName])) {
+			tasks.push([taskName, <string>((<any>taskRepo)[taskName][taskType])])
+		}
+	})
+	return tasks
+}
+
+function determineRoute(pathname: string): string {
+	let route = ""
+	if (pathname.startsWith("/init")) {
+		route = "init"
+	} else if (pathname.startsWith("/command")) {
+		route = "command"
+	} else if (pathname.startsWith("/before")) {
+		route = "before"
+	} else if (pathname.startsWith("/yaml")) {
+		route = "yaml"
+	}
+	return route
+}
+
+function determineTaskNames(searchParams: URLSearchParams): string[] {
+	return ["base"].concat(searchParams.get("tasks")?.split(" ") || [])
 }
 
 export default {
@@ -52,29 +105,24 @@ export default {
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
-		//return new Response(JSON.stringify(actions))
 		const { pathname, searchParams } = new URL(request.url);
-		let taskType = ""
-		if (pathname.startsWith("/init")) {
-			taskType = "init"
-		} else if (pathname.startsWith("/command")) {
-			taskType = "command"
-		} else if (pathname.startsWith("/before")) {
-			taskType = "before"
+		const route = determineRoute(pathname)
+		if (route == "") {
+			return new Response("Unknown route", { status: 404 })
 		}
-		if (taskType == "") {
-			return new Response("Unknown action", { status: 404 })
+		const taskNames = determineTaskNames(searchParams)
+		if (route == "yaml") {
+			return new Response(
+				generateYaml(
+					request.url,
+					getCommandsFromRepo("before", taskNames),
+					getCommandsFromRepo("init", taskNames),
+					getCommandsFromRepo("command", taskNames)
+				)
+			)
 		}
-		const taskNames = ["base"].concat(searchParams.get("tasks")?.split(" ") || [])
-		// return new Response(JSON.stringify(taskNames))
-
-		const tasks: [string, string][] = []
-		taskNames.forEach((taskName) => {
-			if ((taskName in allTasks) && (taskType in (<any>allTasks)[taskName])) {
-				tasks.push([taskName, <string>((<any>allTasks)[taskName][taskType])])
-			}
-		})
-		// const tasks: [string, string][] = taskNames.map((name) => [name, ((allTasks as any)[name] || {})[taskType] || []])
-		return new Response(buildScript(request.url, tasks))
+		const taskType = route
+		const commands = getCommandsFromRepo(taskType, taskNames)
+		return new Response(generateTaskElementBash(request.url, commands))
 	},
 };
